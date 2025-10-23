3043977cafcdd4d7767c89a1696fd32f6e8ea374
Rebe HC <hernandezcruzrebeca767@gmail.com>
2025-10-23 11:40:19 -0600

    x

--- Tarea/app/src/App.tsx
+++ Tarea/app/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [error, setError] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
 
-  const URL = "http://localhost:5000/predict"
+  const URL = "https://simulacion-ahqv.onrender.com/predict"
 
   const handleChange = (e: any) => {
     setPassager({
